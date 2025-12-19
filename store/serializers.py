678adcee3389645cd3c16bd60f5efc4e193e7af2
JoaoNettoo <joao_neto100@hotmail.com>
2025-12-19 01:01:43 -0300

    alteração na lista de peças

--- store/serializers.py
+++ store/serializers.py
@@ -14,7 +14,7 @@ class PecaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Peca
-        fields = ['id', 'nome', 'codigo_sku', 'marca', 'modelo_veiculo', 'categoria', 'preco', 'estoque', 'descricao', 'fornecedor', 'fornecedor_id', 'criado_em']
+        fields = ['id', 'nome', 'codigo', 'marca', 'modelo_veiculo', 'categoria', 'preco', 'estoque', 'descricao', 'fornecedor', 'fornecedor_id', 'criado_em']
 
 class ItemPedidoSerializer(serializers.ModelSerializer):
     peca = PecaSerializer(read_only=True)
@@ -26,6 +26,7 @@ class ItemPedidoSerializer(serializers.ModelSerializer):
 
 class PedidoSerializer(serializers.ModelSerializer):
     itens = ItemPedidoSerializer(many=True)
+    total = serializers.DecimalField(max_digits=50, decimal_places=2, read_only=True)
 
     class Meta:
         model = Pedido
@@ -34,16 +35,27 @@ class PedidoSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         itens_data = validated_data.pop('itens')
         pedido = Pedido.objects.create(**validated_data)
+
         total = 0
         for item in itens_data:
             peca = item['peca']
             quantidade = item['quantidade']
             preco_unitario = item['preco_unitario']
-            ItemPedido.objects.create(pedido=pedido, peca=peca, quantidade=quantidade, preco_unitario=preco_unitario)
-            total += float(preco_unitario) * int(quantidade)
+
+            ItemPedido.objects.create(
+                pedido=pedido,
+                peca=peca,
+                quantidade=quantidade,
+                preco_unitario=preco_unitario
+            )
+
+            total += preco_unitario * quantidade
+
             if peca and peca.estoque is not None:
                 peca.estoque = max(0, peca.estoque - quantidade)
                 peca.save()
+
         pedido.total = total
         pedido.save()
         return pedido
+
